b5c14f2244e9935f2ae06104f688b2a97f657aa2
Bozoo <bozoorak@gmail.com>
2013-04-28 12:10:30 +0200

    alignement à gauche + test sur le mot "distro"

--- src/wordpress/wp-content/themes/tdsimple-child/header.php
+++ src/wordpress/wp-content/themes/tdsimple-child/header.php
@@ -28,8 +28,9 @@
 	<header id="masthead" class="site-header row" role="banner">
 		<div class="twelve columns brand">
 			<hgroup>
-				<h1 class="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+								<h1 class="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+
 			</hgroup>
 		</div>
 			
