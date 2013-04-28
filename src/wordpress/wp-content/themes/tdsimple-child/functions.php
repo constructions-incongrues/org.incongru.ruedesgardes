19317104826beeeb2eb38adabbcd19ff3849d0b6
Tristan Rivoallan <tristan@rivoallan.net>
2013-04-28 08:50:14 +0200

    Amélioration de l'apparence des fiches produit

---
+++ src/wordpress/wp-content/themes/tdsimple-child/functions.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Displays text after the product summary on product page.
+ *
+ * @template woocommerce/templates/content-single-product.php:49
+ */
+add_action('woocommerce_single_product_summary', function() {
+	$html = <<<EOT
+	<p class="contact">
+		Contact : 
+		<a title="Contacter la Rue des Gardes au sujet de cet article" target="_blank" href="mailto:%s <%s>?subject=À propos de %s">%s</a>
+	</p>
+	<hr class="narrow" />
+EOT;
+	
+	$email = get_option('admin_email', 'ruedesgardes@incongru.org');
+	$name = get_option('blogname', 'Rue des Gardes');
+	echo sprintf($html, $name, $email, $_SERVER['REQUEST_URI'], $email);
+});
+
+add_action('wp_enqueue_scripts', function() {
+	// Register fonts stylesheets
+	wp_register_style(
+		'font-ubuntu',
+		'http://fonts.googleapis.com/css?family=Ubuntu:bold',
+		array(),
+		date('Ymd'),
+		'all'
+	);
+	wp_register_style(
+		'font-vollkorn',
+		'http://fonts.googleapis.com/css?family=Vollkorn',
+		array(),
+		date('Ymd'),
+		'all'
+	);
+
+	// Enqueue registered styles
+	wp_enqueue_style('font-ubuntu');
+	wp_enqueue_style('font-vollkorn');
+});
+
+/**
+ * Replaces the ugly "Produits" word with something more acceptable.
+ */
+add_filter('post_type_archive_title', function($title) {
+	if ($title == 'Produits') {
+		return 'Distro';
+	} else {
+		return $title;
+	}
+});
