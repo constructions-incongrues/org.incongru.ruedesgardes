b7a623f40a7057bfeacf22976125fcb66657cc25
Tristan Rivoallan <tristan@rivoallan.net>
2013-09-12 00:06:12 +0200

    [plugins] mise à jour de WooCommerce

--- src/wordpress/wp-content/themes/tdsimple/content.php
+++ src/wordpress/wp-content/themes/tdsimple/content.php
@@ -6,7 +6,7 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('post-index'); ?>>
-	
+
 	<header class="entry-header">
 		<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'tdsimple' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 
@@ -53,29 +53,29 @@
 				<?php printf( __( 'Tagged %1$s', 'tdsimple' ), $tags_list ); ?>
 			</span>
 			<?php endif; // End if $tags_list ?>
-			
-			<?php 
+
+			<?php
 				//check if post title empty
 				$title = get_the_title();
 				if( empty($title) ) {
 					echo "/ <a href=".get_permalink().">". __( 'Read More', 'tdsimple' )."</a>";
 				}
-			?> 
-			
+			?>
+
 		<?php endif; // End if 'post' == get_post_type() ?>
 
 		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-			
+
 			<?php if( get_post_type() != "page" ): ?>
 				<span class="sep"> / </span>
 			<?php endif; ?>
-			
+
 		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'tdsimple' ), __( '1 Comment', 'tdsimple' ), __( '% Comments', 'tdsimple' ) ); ?></span>
 		<?php endif; ?>
 
 		<?php edit_post_link( __( 'Edit', 'tdsimple' ), '<span class="sep"> / </span> <span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-meta -->
-	
+
 	<div class="post-seperator"></div>
-	
+
 </article><!-- #post-<?php the_ID(); ?> -->
