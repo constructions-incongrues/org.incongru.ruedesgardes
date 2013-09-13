7d9f1098e32c1e2f446f4254d7f1df8ed6a547eb
Tristan Rivoallan <tristan@rivoallan.net>
2013-09-13 17:19:24 +0200

    [catalogue] Liste des tags pour filtrer (à styler)

---
+++ src/wordpress/wp-content/themes/tdsimple-child/woocommerce/loop/loop-start.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Product Loop Start
+ *
+ * @author    WooThemes
+ * @package   WooCommerce/Templates
+ * @version     2.0.0
+ */
+?>
+<h1>Trier</h1>
+<ul>
+  <li><a href="<?php echo get_option("siteurl") ?>">Tout</a></li>
+<?php foreach (get_terms('product_tag') as $term): ?>
+  <li>
+    <a href="<?php echo get_term_link($term, 'product_tag') ?>"><?php echo $term->name ?></a>
+  </li>
+<?php endforeach ?>
+</ul>
+
+<h1>Catalogue</h1>
+<ul class="products">
