39da05c223b9bf632cdaf0ff59dd774549520978
Tristan Rivoallan <tristan@rivoallan.net>
2013-11-19 22:40:21 +0100

    [products list] amélioration de l'affichage des catégories

--- src/wordpress/wp-content/themes/tdsimple-child/woocommerce/loop/price.php
+++ src/wordpress/wp-content/themes/tdsimple-child/woocommerce/loop/price.php
@@ -2,14 +2,15 @@
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 global $product;
-$categories = explode(',', strip_tags($product->get_categories()));
-unset($categories[0]);
+$categories = array_filter(explode(',', strip_tags($product->get_categories())), function($data) {
+    return strtolower(trim($data)) != 'stock';
+})
 ?>
 
 <?php if ( $price_html = $product->get_price_html() ) : ?>
     <span class="price">
     <?php if (count($categories)): ?>
-        <?php echo $categories[1] ?> / <?php echo $price_html; ?>
+        <?php echo implode(', ', $categories) ?> / <?php echo $price_html; ?>
     <?php else: ?>
         <?php echo $price_html; ?>
     <?php endif ?>
