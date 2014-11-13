f433f1056470dfa8532fe0a2f6a45de2601cf479
Tristan Rivoallan <tristan@rivoallan.net>
2014-11-13 14:33:15 +0100

    [shop] extraction des catégories "types" (cd, k7, etc)

--- src/wordpress/wp-content/themes/tdsimple-child/woocommerce/loop/loop-start.php
+++ src/wordpress/wp-content/themes/tdsimple-child/woocommerce/loop/loop-start.php
@@ -6,14 +6,38 @@
  * @package   WooCommerce/Templates
  * @version     2.0.0
  */
+$mainCategories = array(
+    'affiche'    => 'Affiche',
+    'cd'         => 'CD',
+    'colifichet' => 'Colifichet',
+    'dvd'        => 'DVD',
+    'k7'         => 'K7',
+    'livre'      => 'Livre',
+    'tissu'      => 'Tissu',
+    'vinyl'      => 'Vinyl'
+);
 ?>
 <h1 class="woocommerce-heading-1">Trier</h1>
 <ul class="woocommerce-tag-list">
-  <li><a href="<?php echo get_option("siteurl/distro/stock/") ?>">Tout</a></li>
+    <li><a href="<?php echo get_option("siteurl/distro/stock/") ?>">Tout</a></li>
+<?php foreach ($mainCategories as $name => $label): ?>
+    <li>
+        <a href="<?php echo get_option("siteurl/distro/stock/product-tag/<?php echo $name ?>") ?>"><?php echo $label ?></a>
+    </li>
+<?php endforeach; ?>
+</ul>
+
+<br />
+<br />
+<br />
+
+<ul class="woocommerce-tag-list">
 <?php foreach (get_terms('product_tag') as $term): ?>
-  <li>
-    <a href="<?php echo get_term_link($term, 'product_tag') ?>"><?php echo $term->name ?></a>
-  </li>
+    <?php if (!in_array($term, array_keys($mainCategories))): ?>
+    <li>
+        <a href="<?php echo get_term_link($term, 'product_tag') ?>"><?php echo $term->name ?></a>
+    </li>
+    <?php endif ?>
 <?php endforeach ?>
 </ul>
 
